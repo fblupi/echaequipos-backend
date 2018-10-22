dd077882299a72fde9af250d804b4b4d7755b379
Fran Bolívar <francisco.bolivar@nazaries.com>
2018-10-22 20:55:20 +0200

    Affiliation model tests #17

---
+++ spec/models/affiliation_spec.rb
@@ -0,0 +1,76 @@
+require 'rails_helper'
+
+RSpec.describe Affiliation, type: :model do
+  describe 'validations' do
+    before(:each) do
+      @user = build(:v1_user)
+      @other_user = build(:v1_user)
+      @group = build(:v1_group)
+    end
+
+    it 'affiliation_type is required' do
+      expect { Affiliation.create!(user: @user, group: @group) }.to raise_error(ActiveRecord::RecordInvalid)
+    end
+
+    it 'user is required' do
+      expect { Affiliation.create!(group: @group, affiliation_type: 'normal') }.to raise_error(ActiveRecord::RecordInvalid)
+    end
+
+    it 'group is required' do
+      expect { Affiliation.create!(user: @user, affiliation_type: 'normal') }.to raise_error(ActiveRecord::RecordInvalid)
+    end
+
+    it 'uniqueness of pair user group required' do
+      expect { Affiliation.create!(user: @user, group: @group, affiliation_type: 'normal') }.to change(Affiliation, :count).by(1)
+      expect { Affiliation.create!(user: @other_user, group: @group, affiliation_type: 'normal') }.to change(Affiliation, :count).by(1)
+      expect { Affiliation.create!(user: @user, group: @group, affiliation_type: 'normal') }.to raise_error(ActiveRecord::RecordInvalid)
+    end
+  end
+
+  describe 'affiliation types' do
+    before(:each) do
+      @affiliation_invitation = build(:v1_affiliation, affiliation_type: 'invitation')
+      @affiliation_normal = build(:v1_affiliation, affiliation_type: 'normal')
+      @affiliation_admin = build(:v1_affiliation, affiliation_type: 'admin')
+    end
+
+    it 'checks invitation' do
+      expect(@affiliation_invitation.invitation?).to be true
+      expect(@affiliation_normal.invitation?).to be false
+      expect(@affiliation_admin.invitation?).to be false
+    end
+
+    it 'checks normal' do
+      expect(@affiliation_invitation.normal?).to be false
+      expect(@affiliation_normal.normal?).to be true
+      expect(@affiliation_admin.normal?).to be false
+    end
+
+    it 'checks admin' do
+      expect(@affiliation_invitation.admin?).to be false
+      expect(@affiliation_normal.admin?).to be false
+      expect(@affiliation_admin.admin?).to be true
+    end
+  end
+
+  describe 'change types' do
+    before(:each) do
+      @affiliation = build(:v1_affiliation)
+    end
+
+    it 'accepts invitation' do
+      @affiliation.accept_invitation
+      expect(@affiliation.affiliation_type).to eq 'normal'
+    end
+
+    it 'upgrades admin' do
+      @affiliation.upgrade_admin
+      expect(@affiliation.affiliation_type).to eq 'admin'
+    end
+
+    it 'downgrades admin' do
+      @affiliation.downgrade_normal
+      expect(@affiliation.affiliation_type).to eq 'normal'
+    end
+  end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -8,4 +8,49 @@ RSpec.describe User, type: :model do
       expect { User.create!(email: 'test@test.com', password: '123456') }.to raise_error(ActiveRecord::RecordInvalid)
     end
   end
+
+  describe 'group creation' do
+    before(:each) do
+      @user = build(:v1_user)
+    end
+
+    it 'does not create a group if the mandatory params are missing' do
+      expect(@user.create_group).to be_nil
+      expect(@user.create_group(location: 'Granada')).to be_nil
+    end
+
+    it 'creates a group' do
+      expect(Affiliation.count).to eq 0
+      expect(Group.count).to eq 0
+
+      @user.create_group(name: 'Notingan Prisa', location: 'Granada')
+
+      expect(Affiliation.count).to eq 1
+      expect(Group.count).to eq 1
+    end
+  end
+
+  describe 'check affiliations' do
+    before(:each) do
+      @user = build(:v1_user)
+      @group_invitation = build(:v1_group)
+      @group_invitation_other = build(:v1_group)
+      @group_normal = build(:v1_group)
+      @group_normal_other = build(:v1_group)
+      @group_admin = build(:v1_group)
+      Affiliation.create(user: @user, group: @group_invitation, affiliation_type: 'invitation')
+      Affiliation.create(user: @user, group: @group_invitation_other, affiliation_type: 'invitation')
+      Affiliation.create(user: @user, group: @group_normal, affiliation_type: 'normal')
+      Affiliation.create(user: @user, group: @group_normal_other, affiliation_type: 'normal')
+      Affiliation.create(user: @user, group: @group_admin, affiliation_type: 'admin')
+    end
+
+    it 'checks current affiliations' do
+      expect(@user.current_affiliations.count).to be(3)
+    end
+
+    it 'check current invitations' do
+      expect(@user.invitation_affiliations.count).to be(2)
+    end
+  end
 end
