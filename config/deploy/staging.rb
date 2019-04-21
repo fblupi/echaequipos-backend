2338d58dd657505a60644158e14d43db39c65ffb
Fran Bolívar <francisco.bolivar@nazaries.com>
2019-04-21 22:23:11 +0200

    passenger_restart_conmmand

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -3,4 +3,4 @@ server '192.168.1.21', user: 'pi', roles: %w[app db]
 set :deploy_to, '/home/pi'
 set :rails_env, 'production'
 set :branch, ENV['BRANCH'] || 'develop'
-set :passenger_restart_command, '-i passenger-config restart-app'
+set :passenger_restart_command, 'passenger-config restart-app'
